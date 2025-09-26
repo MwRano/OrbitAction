c7d29a1a507d803140bf4d122beea31162d3e962
MwRano <hitoki0327@icloud.com>
2025-09-26 16:01:42 +0900

    feat : Add FallState script

---
+++ Assets/Player/Scripts/FallState.cs
@@ -0,0 +1,21 @@
+#nullable enable
+using UnityEngine;
+
+public class FallState : IPlayerState
+{
+    public void Enter()
+    {
+        
+    }
+
+    public void Update()
+    {
+        
+    }
+
+    public void Exit()
+    {
+        
+    }
+
+}
