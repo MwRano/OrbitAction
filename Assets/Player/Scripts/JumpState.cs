c07bf9ec7d0c35345382bb97ba3e357dcb1dfa42
MwRano <hitoki0327@icloud.com>
2025-09-26 20:58:07 +0900

    feat(JumpState) : Add State transition process

--- Assets/Player/Scripts/JumpState.cs
+++ Assets/Player/Scripts/JumpState.cs
@@ -1,16 +1,43 @@
 #nullable enable
 using UnityEngine;
+using VContainer;
 
 public class JumpState : IPlayerState
 {
+    private readonly PlayerController _player;
+
+    [Inject]
+    public JumpState(PlayerController playerController)
+    {
+        _player = playerController;
+    }
+
     public void Enter()
     {
-        
+
     }
 
     public void Update()
     {
-        
+        if (_player.IsGrounded)
+        {
+            // Walkへの遷移
+            if (Mathf.Abs(_player.RigidBody.linearVelocityX) > 0.1f)
+            {
+                _player.StateMachine.TransitionTo(_player.StateMachine.Walk);
+            }
+            // Idleへの遷移
+            else
+            {
+                _player.StateMachine.TransitionTo(_player.StateMachine.Idle);
+            }
+        }
+
+        // Fallへの遷移
+        if (_player.RigidBody.linearVelocityY < -0.1f)
+        {
+            _player.StateMachine.TransitionTo(_player.StateMachine.Fall);
+        }
     }
 
     public void Exit()
