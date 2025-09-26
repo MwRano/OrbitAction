25151b5bcf2829e612a7ca41c88f4b8ec3ea8b3c
MwRano <hitoki0327@icloud.com>
2025-09-26 20:01:33 +0900

    feat : Add constructor to PlayerStateMachine for managemant of player state

--- Assets/Player/Scripts/PlayerStateMachine.cs
+++ Assets/Player/Scripts/PlayerStateMachine.cs
@@ -1,12 +1,30 @@
 #nullable enable
 
+using VContainer;
+
 /// <summary>
 /// Playerの状態を管理するステートマシン
 /// </summary>
 public class PlayerStateMachine
 {
     public IPlayerState? CurrentState { get; private set; }
+    public IdleState Idle { get; }
+    public WalkState Walk { get; }
+    public JumpState Jump { get; }
+    public FallState Fall { get; }
 
+    [Inject]
+    public PlayerStateMachine(
+        IdleState idleState,
+        WalkState walkState,
+        JumpState jumpState,
+        FallState fallState)
+    {
+        Idle = idleState;
+        Walk = walkState;
+        Jump = jumpState;
+        Fall = fallState;
+    }
 
     public void Initialize(IPlayerState startingState)
     {
