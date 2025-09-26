f9b37b9cd073f67fde2749029b3f244bfe448431
MwRano <hitoki0327@icloud.com>
2025-09-26 20:07:24 +0900

    feat(IdleState) : Add state transition process

--- Assets/Player/Scripts/IdleState.cs
+++ Assets/Player/Scripts/IdleState.cs
@@ -1,16 +1,43 @@
 #nullable enable
 using UnityEngine;
+using VContainer;
 
 public class IdleState : IPlayerState
 {
+    private readonly PlayerController _player;
+
+    [Inject]
+    public IdleState(PlayerController playerController)
+    {
+        _player = playerController;
+    }
+
     public void Enter()
     {
-        
+
     }
 
     public void Update()
     {
+        if (_player.IsGrounded == false)
+        {
+            // Jumpへの遷移
+            if (_player.RigidBody.linearVelocityY > 0.1f)
+            {
+                _player.StateMachine.TransitionTo(_player.StateMachine.Jump);
+            }
+            // Fallへの遷移
+            else if (_player.RigidBody.linearVelocityY < -0.1f)
+            {
+                _player.StateMachine.TransitionTo(_player.StateMachine.Fall);
+            }
+        }
         
+        // Walkへの遷移
+        if (Mathf.Abs(_player.RigidBody.linearVelocityX) > 0.1f)
+        {
+            _player.StateMachine.TransitionTo(_player.StateMachine.Walk);
+        }
     }
 
     public void Exit()
