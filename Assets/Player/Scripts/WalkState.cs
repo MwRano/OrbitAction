ade5a1dcc8630969b72dc9765ea0682e53bbdbf3
MwRano <hitoki0327@icloud.com>
2025-09-26 11:18:54 +0900

    feat : Add WalkState script

---
+++ Assets/Player/Scripts/WalkState.cs
@@ -0,0 +1,21 @@
+#nullable enable
+using UnityEngine;
+
+public class WalkState : IPlayerState
+{
+    public void Enter()
+    {
+        
+    }
+
+    public void Update()
+    {
+        
+    }
+
+    public void Exit()
+    {
+        
+    }
+
+}
