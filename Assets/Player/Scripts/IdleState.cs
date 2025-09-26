bde95321fc31af3c0a58dfe6de08a130965c404d
MwRano <hitoki0327@icloud.com>
2025-09-26 11:16:46 +0900

    feat : Add IdleState Script

---
+++ Assets/Player/Scripts/IdleState.cs
@@ -0,0 +1,21 @@
+#nullable enable
+using UnityEngine;
+
+public class IdleState : IPlayerState
+{
+    public void Enter()
+    {
+        
+    }
+
+    public void Update()
+    {
+        
+    }
+
+    public void Exit()
+    {
+        
+    }
+
+}
