c8513c1a7bcaabe62c95df1544f366a7bcf90fb0
MwRano <hitoki0327@icloud.com>
2025-09-29 23:24:06 +0900

    feat : Add PlanetParams scriptable object for planetary attributes

---
+++ Assets/Planet/Params/PlanetParams.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "PlanetParams", menuName = "Scriptable Objects/PlanetParams")]
+public class PlanetParams : ScriptableObject
+{
+    [Header("惑星の飛距離")] 
+    [SerializeField] private float launchDistance; // 惑星の飛距離
+    
+    [Header("惑星の引力")]
+    [SerializeField] private float attractionForce;　// 惑星の引力
+    [SerializeField] private float attractionRange;　// 惑星の引力が及ぶ範囲
+    
+    [Header("公転")]
+    [SerializeField] private float orbitalPeriod; // 公転周期
+    [SerializeField] private float orbitalRange; // 公転可能な範囲
+
+    public float LaunchDistance => launchDistance;
+    public float AttractionForce => attractionForce;
+    public float AttractionRange => attractionRange;
+    public float OrbitalPeriod => orbitalPeriod;
+    public float OrbitalRange => orbitalRange;
+}
