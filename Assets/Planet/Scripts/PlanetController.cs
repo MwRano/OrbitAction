4ce82d42c9613c0597468f6f150b5ef86b4b0d32
MwRano <hitoki0327@icloud.com>
2025-10-04 21:14:13 +0900

    feat(PlanetController): Implement PlanetController for player interaction and movement parameters

---
+++ Assets/Planet/Scripts/PlanetController.cs
@@ -0,0 +1,19 @@
+﻿#nullable enable
+using UnityEngine;
+using VContainer;
+
+
+public class PlanetController : MonoBehaviour
+{
+    private PlayerController _player = null!;
+    private float _smoothTime;
+    private float _maxSpeed;
+
+    [Inject]
+    public void Construct(PlayerController playerController, PlanetParams planetParams)
+    {
+        _player = playerController;
+        _smoothTime = planetParams.SmoothTime;
+        _maxSpeed = planetParams.MaxSpeed;
+    }
+}
