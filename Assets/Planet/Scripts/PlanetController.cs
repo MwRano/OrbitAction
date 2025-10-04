061af6dc470286d1ef81aec3e313117c1530302c
MwRano <hitoki0327@icloud.com>
2025-10-05 00:13:02 +0900

    feat(PlanetController): Implement floating motion control based on player velocity

--- Assets/Planet/Scripts/PlanetController.cs
+++ Assets/Planet/Scripts/PlanetController.cs
@@ -1,43 +1,55 @@
 ﻿#nullable enable
 using UnityEngine;
 using VContainer;
+using LitMotion;
+using LitMotion.Extensions;
 
-
+/// <summary>
+/// Planetの挙動を制御するクラス
+/// </summary>
 public class PlanetController : MonoBehaviour
 {
     private PlayerController _player = null!;
-    private float _smoothTime;
-    private float _maxSpeed;
     private Vector2 _currentVelocity;
+    
+    private MotionHandle _floatingMotion;
+    private bool _isFloating = false;
     
     [Inject]
     public void Construct(PlayerController playerController, PlanetParams planetParams)
     {
         _player = playerController;
-        _smoothTime = planetParams.SmoothTime;
-        _maxSpeed = planetParams.MaxSpeed;
     }
+    
     private void Update()
     {
-        Vector2 playerPos = _player.transform.position;
-        Vector2 targetPosition = _player.IsFacingRight
-            ? new Vector2(playerPos.x - 1, playerPos.y + 1)
-            : new Vector2(playerPos.x + 1, playerPos.y + 1);
-        FollowTarget(targetPosition);
+        ControlFloatingMotion();
     }
     
     /// <summary>
-    /// 目標に追従するメソッド
+    /// 浮遊モーションの制御
     /// </summary>
-    /// <param name="targetPosition"></param>
-    private void FollowTarget(Vector2 targetPosition)
+    private void ControlFloatingMotion()
     {
-        transform.position = Vector2.SmoothDamp(
-            transform.position,
-            targetPosition,
-            ref _currentVelocity,
-            _smoothTime,
-            _maxSpeed
-        );
+        bool shouldFloat = _player.Rigidbody.linearVelocity.magnitude < 0.2f && _currentVelocity.magnitude < 0.1f;
+        if (!_isFloating && shouldFloat) 
+        {
+            _floatingMotion = StartFloatingMotion();
+            _isFloating = true;
+        }
+        else if(_isFloating && !shouldFloat)
+        {
+            _floatingMotion.Cancel();
+            _isFloating = false;
+        }
+    }
+
+    private MotionHandle StartFloatingMotion()
+    {
+        return LMotion.Create(transform.position.y, transform.position.y - 0.2f, 1f)
+                .WithEase(Ease.InOutSine)
+                .WithLoops(-1, LoopType.Yoyo)
+                .BindToPositionY(transform)
+                .AddTo(transform);
     }
 }
