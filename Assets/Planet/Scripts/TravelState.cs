04a6aa1efff1616dd43d3b323f49f496e1c92200
MwRano <hitoki0327@icloud.com>
2025-10-05 19:52:24 +0900

    feat(TravelState): Implement TravelState class for player launch mechanics

---
+++ Assets/Planet/Scripts/TravelState.cs
@@ -0,0 +1,36 @@
+﻿#nullable enable
+using LitMotion;
+using LitMotion.Extensions;
+using UnityEngine;
+using VContainer;
+
+public class TravelState : IPlanetState
+{
+    private IPlayerContext _player;
+    float _launchDistance;
+    
+    [Inject]
+    public TravelState(IPlayerContext player, PlanetParams planetParams)
+    {
+        _player = player;
+        _launchDistance = planetParams.LaunchDistance;
+    }
+    public void Enter(IPlanetContext planet)
+    {
+        Vector2 destPos = _launchDistance * _player.LookingDirection + (Vector2)planet.PlanetTransform.position;
+        LMotion.Create((Vector2)planet.PlanetTransform.position, destPos, 0.5f)
+            .WithEase(Ease.OutCubic)
+            .BindToPositionXY(planet.PlanetTransform)
+            .AddTo(planet.PlanetTransform);
+    }
+
+    public void Update(IPlanetContext planet, PlanetStateMachine stateMachine)
+    {
+        
+    }
+
+    public void Exit()
+    {
+        
+    }
+}
