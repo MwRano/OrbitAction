896943776a8a79377aabf2835a694ad055de181d
MwRano <hitoki0327@icloud.com>
2025-10-05 20:11:54 +0900

    feat(TravelState): Enhance travel mechanics with launch completion tracking

--- Assets/Planet/Scripts/TravelState.cs
+++ Assets/Planet/Scripts/TravelState.cs
@@ -6,8 +6,9 @@ using VContainer;
 
 public class TravelState : IPlanetState
 {
-    private IPlayerContext _player;
-    float _launchDistance;
+    private readonly IPlayerContext _player;
+    private readonly float _launchDistance;
+    private bool _isReached;
     
     [Inject]
     public TravelState(IPlayerContext player, PlanetParams planetParams)
@@ -20,17 +21,19 @@ public class TravelState : IPlanetState
         Vector2 destPos = _launchDistance * _player.LookingDirection + (Vector2)planet.PlanetTransform.position;
         LMotion.Create((Vector2)planet.PlanetTransform.position, destPos, 0.5f)
             .WithEase(Ease.OutCubic)
+            .WithOnComplete(() => _isReached = true)
             .BindToPositionXY(planet.PlanetTransform)
             .AddTo(planet.PlanetTransform);
     }
 
     public void Update(IPlanetContext planet, PlanetStateMachine stateMachine)
     {
-        
+        // 到達すればdeployに遷移
+        if (_isReached) stateMachine.TransitionTo(stateMachine.Deploy, planet);
     }
 
     public void Exit()
     {
-        
+        _isReached = false;
     }
 }
