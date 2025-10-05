2fc831a8eb20d75b4818414db01b5b7839b4031a
MwRano <hitoki0327@icloud.com>
2025-10-05 19:41:35 +0900

    feat(FollowState): Implement FollowState class for player tracking in planet context

---
+++ Assets/Planet/Scripts/FollowState.cs
@@ -0,0 +1,45 @@
+﻿#nullable enable
+using UnityEngine;
+using VContainer;
+
+public class FollowState :IPlanetState
+{
+    private IPlayerContext _player;
+    private Vector2 _currentVelocity;
+    private float _smoothTime;
+    private float _maxSpeed;
+
+    [Inject]
+    public FollowState(IPlayerContext player, PlanetParams planetParams)
+    {
+        _player = player;
+        _smoothTime = planetParams.SmoothTime;
+        _maxSpeed = planetParams.MaxSpeed;
+    }
+    
+    public void Enter(IPlanetContext planet)
+    {
+        
+    }
+
+    public void Update(IPlanetContext planet, PlanetStateMachine stateMachine)
+    {
+        Vector2 playerPos = _player.CurrentPosition;
+        Vector2 targetPosition = _player.IsFacingRight　// playerの向きに応じて追従位置を変更
+            ? new Vector2(playerPos.x - 1, playerPos.y + 1)
+            : new Vector2(playerPos.x + 1, playerPos.y + 1);
+        
+        planet.PlanetTransform.position = Vector2.SmoothDamp(
+            planet.PlanetTransform.position,
+            targetPosition,
+            ref _currentVelocity,
+            _smoothTime,
+            _maxSpeed
+        );
+    }
+
+    public void Exit()
+    {
+        
+    }
+}
