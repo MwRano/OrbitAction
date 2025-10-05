5f299d09ab841df93c61b1745a9d0192d27e0fff
MwRano <hitoki0327@icloud.com>
2025-10-05 13:11:31 +0900

    feat(PlanetController): Implement launch functionality and input handling

--- Assets/Planet/Scripts/PlanetController.cs
+++ Assets/Planet/Scripts/PlanetController.cs
@@ -3,35 +3,57 @@ using UnityEngine;
 using VContainer;
 using LitMotion;
 using LitMotion.Extensions;
+using UnityEngine.InputSystem;
 
 /// <summary>
 /// Planetの挙動を制御するクラス
 /// </summary>
 public class PlanetController : MonoBehaviour
 {
+    private InputSystemActions _inputSystemActions = null!;
     private PlayerController _player = null!;
+    
+    // 追従モーションのパラメータ
     private float _smoothTime;
     private float _maxSpeed;
     private Vector2 _currentVelocity;
     
+    // launchのパラメータ
+    private float _launchDistance;
+    
+    // Motionのフラグ
     private MotionHandle _floatingMotion;
-    private bool _isFloating = false;
+    private bool _isFloating;
+    private bool _isOwnedByPlayer;
     
     [Inject]
-    public void Construct(PlayerController playerController, PlanetParams planetParams)
+    public void Construct(
+        PlayerController playerController, 
+        PlanetParams planetParams,
+        InputSystemActions inputSystemActions)
     {
         _player = playerController;
         _smoothTime = planetParams.SmoothTime;
         _maxSpeed = planetParams.MaxSpeed;
+        _launchDistance = planetParams.LaunchDistance;
+        _inputSystemActions = inputSystemActions;
     }
 
     private void Awake()
     {
+        // InputSystemへのメソッド登録
+        _inputSystemActions.Player.Launch.performed += Launch;
+        _inputSystemActions.Player.Enable();
+        
+        // フラグの初期化
+        _isOwnedByPlayer = true;
+        _isFloating = false;
         StartRotationMotion();
     }
     
     private void Update()
     {
+        // Motionの制御
         ControlFloatingMotion();
         ControlFollowingMotion();
     }
@@ -50,13 +72,14 @@ public class PlanetController : MonoBehaviour
     /// </summary>
     private void ControlFloatingMotion()
     {
-        bool shouldFloat = _player.Rigidbody.linearVelocity.magnitude < 0.2f && _currentVelocity.magnitude < 0.1f;
-        if (!_isFloating && shouldFloat) 
+        bool shouldFloat = _player.Rigidbody.linearVelocity.sqrMagnitude < 0.2f && _currentVelocity.sqrMagnitude < 0.1f || !_isOwnedByPlayer;
+        
+        if (!_isFloating && shouldFloat) // プレイヤーが低速な場合に浮遊モーションを開始
         {
             _floatingMotion = StartFloatingMotion();
             _isFloating = true;
         }
-        else if(_isFloating && !shouldFloat)
+        else if(_isFloating && !shouldFloat)　// プレイヤーが高速な場合に浮遊モーションを停止
         {
             _floatingMotion.Cancel();
             _isFloating = false;
@@ -77,12 +100,13 @@ public class PlanetController : MonoBehaviour
     /// </summary>
     private void ControlFollowingMotion()
     {
+        // Playerから少し離れた位置に追従
         Vector2 playerPos = _player.transform.position;
-        Vector2 targetPosition = _player.IsFacingRight
+        Vector2 targetPosition = _player.IsFacingRight　// playerの向きに応じて追従位置を変更
             ? new Vector2(playerPos.x - 1, playerPos.y + 1)
             : new Vector2(playerPos.x + 1, playerPos.y + 1);
         
-        if(!_isFloating) FollowTarget(targetPosition);
+        if(!_isFloating && _isOwnedByPlayer) FollowTarget(targetPosition);
     }
     
     private void FollowTarget(Vector2 targetPosition)
@@ -94,5 +118,24 @@ public class PlanetController : MonoBehaviour
             _smoothTime,
             _maxSpeed
         );
+    }
+    
+    /// <summary>
+    /// 指定方向に惑星を飛ばすメソッド
+    /// </summary>
+    /// <param name="context"></param>
+    private void Launch(InputAction.CallbackContext context)
+    {
+        if (!_isOwnedByPlayer) return;
+        
+        _isOwnedByPlayer = false;
+        if(_floatingMotion.IsActive())_floatingMotion.Cancel();
+        
+        Vector2 destPos = _launchDistance * _player.LookingDirection + (Vector2)transform.position;
+        LMotion.Create((Vector2)transform.position, destPos, 0.5f)
+            .WithEase(Ease.OutCubic)
+            .WithOnComplete(()=> _isFloating = false)
+            .BindToPositionXY(transform)
+            .AddTo(transform);
     }
 }
