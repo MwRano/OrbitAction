81165f91eefaaaadc35e699246d9530395e6eda9
MwRano <hitoki0327@icloud.com>
2025-10-05 20:15:08 +0900

    feat(DeployState): Refactor floating motion handling and add state transition logic

--- Assets/Planet/Scripts/DeployState.cs
+++ Assets/Planet/Scripts/DeployState.cs
@@ -4,7 +4,8 @@ using LitMotion.Extensions;
 
 public class DeployState : IPlanetState
 {
-    MotionHandle _floatingMotion;
+    private MotionHandle _floatingMotion;
+    
     public void Enter(IPlanetContext planet)
     {
         _floatingMotion = LMotion.Create(planet.PlanetTransform.position.y, planet.PlanetTransform.position.y - 0.2f, 1f)
@@ -16,11 +17,12 @@ public class DeployState : IPlanetState
 
     public void Update(IPlanetContext planet, PlanetStateMachine stateMachine)
     {
-        
+        // 状態遷移の判定
+        if(!planet.IsLaunched) stateMachine.TransitionTo(stateMachine.Follow, planet);
     }
 
     public void Exit()
     {
-        
+        _floatingMotion.Cancel();
     }
 }
