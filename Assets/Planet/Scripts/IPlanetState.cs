ba5bc12ae3ce25512a2300a0c89eca60a99eaebd
MwRano <hitoki0327@icloud.com>
2025-10-05 18:38:38 +0900

    feat(PlanetStateMachine): Implement IPlanetState interface and state machine for planet management

---
+++ Assets/Planet/Scripts/IPlanetState.cs
@@ -0,0 +1,6 @@
+﻿public interface IPlanetState
+{
+    public void Enter(IPlanetContext planet);
+    public void Update(IPlanetContext planet, PlanetStateMachine stateMachine);
+    public void Exit();
+}
---
+++ Assets/Planet/Scripts/PlanetStateMachine.cs
@@ -0,0 +1,25 @@
+﻿#nullable enable
+using VContainer;
+
+public class PlanetStateMachine
+{
+    private IPlanetState _currentState = null!;
+    
+    public void Initialize(IPlanetState startingState, IPlanetContext planet)
+    {
+        _currentState = startingState;
+        startingState.Enter(planet);
+    }
+
+    public void TransitionTo(IPlanetState nextState, IPlanetContext planet)
+    {
+        _currentState.Exit();
+        _currentState = nextState;
+        nextState.Enter(planet);
+    }
+
+    public void Update(IPlanetContext planet)
+    {
+        _currentState.Update(planet, this);
+    }
+}
