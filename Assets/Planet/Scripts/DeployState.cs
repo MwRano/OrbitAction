da7b51e6685782d1e1b01e78070238ca7f2f00d4
MwRano <hitoki0327@icloud.com>
2025-10-05 19:54:46 +0900

    feat(DeployState): Implement DeployState class for floating motion in planet context

---
+++ Assets/Planet/Scripts/DeployState.cs
@@ -0,0 +1,26 @@
+﻿#nullable enable
+using LitMotion;
+using LitMotion.Extensions;
+
+public class DeployState : IPlanetState
+{
+    MotionHandle _floatingMotion;
+    public void Enter(IPlanetContext planet)
+    {
+        _floatingMotion = LMotion.Create(planet.PlanetTransform.position.y, planet.PlanetTransform.position.y - 0.2f, 1f)
+            .WithEase(Ease.InOutSine)
+            .WithLoops(-1, LoopType.Yoyo)
+            .BindToPositionY(planet.PlanetTransform)
+            .AddTo(planet.PlanetTransform);
+    }
+
+    public void Update(IPlanetContext planet, PlanetStateMachine stateMachine)
+    {
+        
+    }
+
+    public void Exit()
+    {
+        
+    }
+}
