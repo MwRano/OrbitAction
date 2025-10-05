c8c7e8bbe09b8718ac345c7ff701db8f71cb3231
MwRano <hitoki0327@icloud.com>
2025-10-05 19:57:54 +0900

    feat(PlanetStateMachine): Add state properties and constructor for state injection

--- Assets/Planet/Scripts/PlanetStateMachine.cs
+++ Assets/Planet/Scripts/PlanetStateMachine.cs
@@ -5,6 +5,24 @@ public class PlanetStateMachine
 {
     private IPlanetState _currentState = null!;
     
+    public HoverState Hover { get; }
+    public FollowState Follow { get; }
+    public TravelState Travel { get; }
+    public DeployState Deploy { get; }
+
+    [Inject]
+    public PlanetStateMachine(
+        HoverState hoverState,
+        FollowState followState,
+        TravelState travelState,
+        DeployState deployState)
+    {
+        Hover = hoverState;
+        Follow = followState;
+        Travel = travelState;
+        Deploy = deployState;
+    }
+
     public void Initialize(IPlanetState startingState, IPlanetContext planet)
     {
         _currentState = startingState;
