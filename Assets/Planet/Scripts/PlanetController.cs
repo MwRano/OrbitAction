5522709a3cb2459938908886bea4818800dc1198
MwRano <hitoki0327@icloud.com>
2025-10-08 16:50:18 +0900

    feat(PlanetController): Add orbit area view and sprite renderer to IPlanetContext

--- Assets/Planet/Scripts/PlanetController.cs
+++ Assets/Planet/Scripts/PlanetController.cs
@@ -9,24 +9,11 @@ using UnityEngine.InputSystem;
 public class PlanetController : MonoBehaviour, IPlanetContext
 {
     [SerializeField] private GameObject attractionAreaView = null!;
-    
+    [SerializeField] private GameObject orbitAreaView = null!;
+
     private InputSystemActions _inputSystemActions = null!;
     private PlanetStateMachine _stateMachine = null!;
 
-    public Transform PlanetTransform { get; private set; } = null!;
-    public bool IsLaunched { get; private set; }
-    public SpriteRenderer AttractionAreaSpriteRenderer { get; private set; } = null!;
-    public GameObject AttractionAreaView => attractionAreaView;
-
-    [Inject]
-    public void Construct(
-        InputSystemActions inputSystemActions,
-        PlanetStateMachine stateMachine)
-    {
-        _inputSystemActions = inputSystemActions;
-        _stateMachine = stateMachine;
-    }
-    
     private void Awake()
     {
         // InputSystemへのメソッド登録
@@ -37,6 +24,7 @@ public class PlanetController : MonoBehaviour, IPlanetContext
         PlanetTransform = transform;
         IsLaunched = false;
         AttractionAreaSpriteRenderer = attractionAreaView.GetComponent<SpriteRenderer>();
+        OrbitAreaSpriteRenderer = orbitAreaView.GetComponent<SpriteRenderer>();
         _stateMachine.Initialize(_stateMachine.Follow, this);
     }
 
@@ -45,11 +33,28 @@ public class PlanetController : MonoBehaviour, IPlanetContext
         _stateMachine.Update(this);
     }
 
+    public Transform PlanetTransform { get; private set; } = null!;
+    public bool IsLaunched { get; private set; }
+    public SpriteRenderer AttractionAreaSpriteRenderer { get; private set; } = null!;
+    public SpriteRenderer OrbitAreaSpriteRenderer { get; private set; } = null!;
+    public GameObject AttractionAreaView => attractionAreaView;
+    public GameObject OrbitAreaView => orbitAreaView;
+
+
+    [Inject]
+    public void Construct(
+        InputSystemActions inputSystemActions,
+        PlanetStateMachine stateMachine)
+    {
+        _inputSystemActions = inputSystemActions;
+        _stateMachine = stateMachine;
+    }
+
     private void OnLaunch(InputAction.CallbackContext context)
     {
         IsLaunched ^= true;
     }
-    
+
     private void OnAttract(InputAction.CallbackContext context)
     {
         // DeployStateのときのみAttractを実行
