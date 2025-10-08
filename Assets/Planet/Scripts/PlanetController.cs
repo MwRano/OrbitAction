ae4b1118623125bdc57515040ace0dd518386321
MwRano <noratohira@gmail.com>
2025-10-08 10:44:40 +0900

    feat(PlanetController): Add attraction functionality and integrate with input system

--- Assets/Planet/Scripts/PlanetController.cs
+++ Assets/Planet/Scripts/PlanetController.cs
@@ -8,27 +8,15 @@ using UnityEngine.InputSystem;
 /// </summary>
 public class PlanetController : MonoBehaviour, IPlanetContext
 {
+    [SerializeField] private GameObject attractionAreaView = null!;
+    
     private InputSystemActions _inputSystemActions = null!;
     private PlanetStateMachine _stateMachine = null!;
 
-    private void Awake()
-    {
-        // InputSystemへのメソッド登録
-        _inputSystemActions.Planet.Launch.performed += OnLaunch;
-        _inputSystemActions.Planet.Enable();
-
-        PlanetTransform = transform;
-        IsLaunched = false;
-        _stateMachine.Initialize(_stateMachine.Follow, this);
-    }
-
-    private void Update()
-    {
-        _stateMachine.Update(this);
-    }
-
     public Transform PlanetTransform { get; private set; } = null!;
     public bool IsLaunched { get; private set; }
+    public SpriteRenderer AttractionAreaSpriteRenderer { get; private set; } = null!;
+    public GameObject AttractionAreaView => attractionAreaView;
 
     [Inject]
     public void Construct(
@@ -38,9 +26,36 @@ public class PlanetController : MonoBehaviour, IPlanetContext
         _inputSystemActions = inputSystemActions;
         _stateMachine = stateMachine;
     }
+    
+    private void Awake()
+    {
+        // InputSystemへのメソッド登録
+        _inputSystemActions.Planet.Launch.performed += OnLaunch;
+        _inputSystemActions.Planet.Attract.performed += OnAttract;
+        _inputSystemActions.Planet.Enable();
+
+        PlanetTransform = transform;
+        IsLaunched = false;
+        AttractionAreaSpriteRenderer = attractionAreaView.GetComponent<SpriteRenderer>();
+        _stateMachine.Initialize(_stateMachine.Follow, this);
+    }
+
+    private void Update()
+    {
+        _stateMachine.Update(this);
+    }
 
     private void OnLaunch(InputAction.CallbackContext context)
     {
         IsLaunched ^= true;
+    }
+    
+    private void OnAttract(InputAction.CallbackContext context)
+    {
+        // DeployStateのときのみAttractを実行
+        if (_stateMachine.CurrentState is DeployState deployState)
+        {
+            deployState.Attract(PlanetTransform.position);
+        }
     }
 }
