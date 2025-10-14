d3b7b1a820c6b3e0bca7aed9a1165828ef523558
MwRano <hitoki0327@icloud.com>
2025-10-14 23:40:41 +0900

    feat(TravelState): Adjust launch motion duration for smoother transitions

--- Assets/Planet/Scripts/TravelState.cs
+++ Assets/Planet/Scripts/TravelState.cs
@@ -6,22 +6,22 @@ using VContainer;
 
 public class TravelState : IPlanetState
 {
-    private readonly IPlayerContext _player;
     private readonly float _launchDistance;
+    private readonly IPlayerContext _player;
     private bool _isReached;
-    
+
     [Inject]
     public TravelState(IPlayerContext player, PlanetParams planetParams)
     {
         _player = player;
         _launchDistance = planetParams.LaunchDistance;
     }
-    
+
     public void Enter(IPlanetContext planet)
     {
         // 移動モーション 
         Vector2 destPos = _launchDistance * _player.LookingDirection + (Vector2)planet.PlanetTransform.position;
-        LMotion.Create((Vector2)planet.PlanetTransform.position, destPos, 0.5f)
+        LMotion.Create((Vector2)planet.PlanetTransform.position, destPos, 0.2f)
             .WithEase(Ease.OutCubic)
             .WithOnComplete(() => _isReached = true)
             .BindToPositionXY(planet.PlanetTransform)
