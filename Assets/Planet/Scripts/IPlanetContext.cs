594194e8943e1953e70602f6bade89bbeb0e2fe6
MwRano <noratohira@gmail.com>
2025-10-17 16:58:55 +0900

    rework(IPlanetContext): Remove AttractionArea properties to streamline planet context interface

--- Assets/Planet/Scripts/IPlanetContext.cs
+++ Assets/Planet/Scripts/IPlanetContext.cs
@@ -2,8 +2,6 @@
 
 public interface IPlanetContext
 {
-    GameObject AttractionAreaView { get; }
-    SpriteRenderer AttractionAreaSpriteRenderer { get; }
     GameObject OrbitAreaView { get; }
     SpriteRenderer OrbitAreaSpriteRenderer { get; }
     Transform PlanetTransform { get; }
