665e5e4aa8ab3161bd5bf172dda4e4bf556f555b
MwRano <noratohira@gmail.com>
2025-10-27 00:06:10 +0900

    feat: Change GameClearDirector to implement IStartable for improved initialization

--- Assets/Game/Scripts/GameClearDirector.cs
+++ Assets/Game/Scripts/GameClearDirector.cs
@@ -3,19 +3,28 @@ using Cysharp.Threading.Tasks;
 using Planet;
 using Player;
 using VContainer;
-using UnityEngine;
 using R3;
+using VContainer.Unity;
 
 namespace Game
 {
     /// <summary>
     /// ゲームクリア時の演出を制御するクラス
     /// </summary>
-    public class GameClearDirector : MonoBehaviour
+    public class GameClearDirector : IStartable
     {
         private PlanetController _planetController = null!;
         private PlanetParams _planetParams = null!;
         private PlayerController _playerController = null!;
+
+        public void Start()
+        {
+            // ゴール地点に到達したらクリア演出を開始
+            Observable.EveryValueChanged(_playerController, p => p.IsGoalReached)
+                .Where(isReached => isReached)
+                .Subscribe(_ => StartClearDirection())
+                .AddTo(_playerController);
+        }
 
         [Inject]
         public void Construct(
@@ -26,12 +35,6 @@ namespace Game
             _playerController = playerController;
             _planetController = planetController;
             _planetParams = planetParams;
-
-            // ゴール地点に到達したらクリア演出を開始
-            Observable.EveryValueChanged(_playerController, p => p.IsGoalReached)
-                .Where(isReached => isReached)
-                .Subscribe(_ => StartClearDirection())
-                .AddTo(this);
         }
 
         private void StartClearDirection()
