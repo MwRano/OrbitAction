29b9d60ac935bbf27b1139eadbfb2931970f1099
MwRano <noratohira@gmail.com>
2025-11-02 15:15:07 +0900

    deploy位置を計算するクラスを作成

---
+++ Assets/Planet/Scripts/DeployPositionCalculator.cs
@@ -0,0 +1,52 @@
+#nullable enable
+using Player;
+using UnityEngine;
+using VContainer;
+using System;
+
+namespace Planet
+{
+    public class DeployPositionCalculator
+    {
+        private readonly PlanetParams _planetParams;
+        private readonly PlayerParam _playerParam;
+
+        [Inject]
+        public DeployPositionCalculator(
+            PlanetParams planetParams,
+            PlayerParam playerParam)
+        {
+            _planetParams = planetParams;
+            _playerParam = playerParam;
+        }
+
+        public Vector2 Calculate(Vector2 playerPos, Vector2 playerLookDir, Vector2 planetPos, float planetRadius)
+        {
+            var hit = Physics2D.Raycast(
+                playerPos,
+                playerLookDir,
+                100,　// 十分に大きな値
+                _playerParam.GroundLayer);
+            Vector2 target = hit.point;
+
+            Vector2 planetToHitDir = (hit.point - planetPos).normalized;
+            float planetTohitDist = Vector2.Distance(hit.point, planetPos);
+
+            float distToGround = Math.Abs(hit.normal.x) > Math.Abs(hit.normal.y)
+                ? Mathf.Abs(hit.point.x - planetPos.x)
+                : Mathf.Abs(hit.point.y - planetPos.y);
+            if (distToGround == 0) return target;
+
+            // 壁や地面にplanetがめり込まないように補正
+            target = hit.point + -planetToHitDir * (planetRadius * (planetTohitDist / distToGround));
+
+            // 発射可能距離を超えていたら補正
+            if (Vector2.Distance(target, playerPos) > _planetParams.LaunchDistance)
+            {
+                target = playerPos + playerLookDir * _planetParams.LaunchDistance;
+            }
+
+            return target;
+        }
+    }
+}
