ba98270bc3a77b92518d60c3a7b61e856a2d042b
MwRano <noratohira@gmail.com>
2025-11-03 11:30:00 +0900

    追従対象をplayerに設定する処理を追加

--- Assets/Game/Scripts/AreaManager.cs
+++ Assets/Game/Scripts/AreaManager.cs
@@ -9,20 +9,19 @@ namespace Game
     /// </summary>
     public class AreaManager : MonoBehaviour
     {
-        [SerializeField]
-        private CinemachineCamera virtualCamera; // このエリアが管理するvcam
-        
-        [SerializeField]
-        private Transform respawnPoint; // リスポーンポイント
-
         private const int ActivePriority = 11; // アクティブ時の優先度
         private const int InactivePriority = 10; // 非アクティブ時の優先度
+
+        [SerializeField] private CinemachineCamera virtualCamera; // このエリアが管理するvcam
+
+        [SerializeField] private Transform respawnPoint; // リスポーンポイント
 
         // プレイヤーがこのエリアに入った時
         private void OnTriggerEnter2D(Collider2D other)
         {
             if (!other.CompareTag("Player")) return;
             virtualCamera.Priority = ActivePriority;
+            virtualCamera.Target.TrackingTarget = other.transform; // ターゲットをプレイヤーに設定
             other.gameObject.GetComponent<PlayerController>().SetRespawnPosition(respawnPoint.position);
         }
 
