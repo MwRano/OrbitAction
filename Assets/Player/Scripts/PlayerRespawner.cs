73cfa62aaef164bf9aafef4b919c3e866ef43c71
MwRano <noratohira@gmail.com>
2025-11-17 15:08:28 +0900

    Playerのリスポーン関連の処理を行うクラスを作成

---
+++ Assets/Player/Scripts/PlayerRespawner.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using R3;
+using UnityEngine;
+
+namespace Player
+{
+    public class PlayerRespawner
+    {
+        private Vector2 _respawnPosition;
+
+        public Action OnRespawn;
+
+        public PlayerRespawner(PlayerCore player)
+        {
+            player.IsDead
+                .Where(isDead => isDead)
+                .Subscribe(_ =>
+                {
+                    player.Rb.simulated = false;
+                    Respawn(player.Rb);
+                });
+        }
+
+        public void SetRespawnPosition(Vector2 respawnPosition)
+        {
+            _respawnPosition = respawnPosition;
+        }
+
+        private void Respawn(Rigidbody2D rb)
+        {
+            rb.simulated = false;
+            Observable.Timer(TimeSpan.FromSeconds(0.635f))
+                .Subscribe(_ =>
+                {
+                    OnRespawn?.Invoke();
+                    rb.transform.position = _respawnPosition;
+                    rb.linearVelocity = Vector2.zero;
+                    rb.simulated = true;
+                });
+        }
+    }
+}
