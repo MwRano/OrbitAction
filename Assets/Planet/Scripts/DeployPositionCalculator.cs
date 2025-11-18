7d15bb692d7327aaa6db6a7d2beaf9afc6ba381e
MwRano <noratohira@gmail.com>
2025-11-18 19:54:43 +0900

    計算量が多いため、距離計算時に平方根の計算をしないように変更

--- Assets/Planet/Scripts/DeployPositionCalculator.cs
+++ Assets/Planet/Scripts/DeployPositionCalculator.cs
@@ -1,4 +1,3 @@
-#nullable enable
 using System;
 using Orbit.Player;
 using UnityEngine;
@@ -23,13 +22,13 @@ namespace Orbit.Planet
         public Vector2 Calculate(Vector2 playerPos, Vector2 playerLookDir, Vector2 planetPos, float planetRadius)
         {
             var hit = Physics2D.Raycast(playerPos, playerLookDir, 100,　_playerParam.GroundLayer);
-            Vector2 target = hit.point;
+            var target = hit.point;
 
-            // 三角形の相似を利用して補正量を計算
-            Vector2 planetToHitDir = (hit.point - planetPos).normalized;
-            float planetToHitDist = Vector2.Distance(hit.point, planetPos);
+            // 補正量を計算
+            var planetToHitDir = (hit.point - planetPos).normalized;
+            var planetToHitDist = Vector2.Distance(hit.point, planetPos);
 
-            float distToGround = Math.Abs(hit.normal.x) > Math.Abs(hit.normal.y)
+            var distToGround = Math.Abs(hit.normal.x) > Math.Abs(hit.normal.y)
                 ? Mathf.Abs(hit.point.x - planetPos.x)
                 : Mathf.Abs(hit.point.y - planetPos.y);
             if (distToGround == 0) return target;
@@ -38,7 +37,7 @@ namespace Orbit.Planet
             target = hit.point + -planetToHitDir * (planetRadius * (planetToHitDist / distToGround));
 
             // 発射可能距離を超えていたら or 壁に到達しないときに補正
-            if (Vector2.Distance(target, playerPos) > _planetParams.LaunchDistance || !hit)
+            if ((target - playerPos).sqrMagnitude > _planetParams.LaunchDistanceSqr || !hit)
             {
                 target = playerPos + playerLookDir * _planetParams.LaunchDistance;
             }
