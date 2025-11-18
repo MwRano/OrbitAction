250a77dce7c70a4491fa52c12226f5628cc0d7dd
MwRano <noratohira@gmail.com>
2025-11-18 20:04:40 +0900

    Orbitの処理は移動させたため、削除

--- Assets/Player/Scripts/PlayerMover.cs
+++ Assets/Player/Scripts/PlayerMover.cs
@@ -1,42 +1,26 @@
-﻿using System;
-using System.Linq;
-using LitMotion;
-using LitMotion.Extensions;
-using Orbit.Planet;
-using R3;
+﻿using R3;
 using UnityEngine;
-using VContainer.Unity;
+using VContainer;
 
 namespace Orbit.Player
 {
-    public class PlayerMover : ITickable
+    public class PlayerMover
     {
-        private readonly PlanetParams _planetParams;
         private readonly PlayerCore _player;
         private readonly PlayerParam _playerParams;
-        private readonly CompositeDisposable _disposable = new();
-        private bool _isOrbiting;
-
-        public PlayerMover(PlayerParam playerParam,
+    
+        [Inject]
+        public PlayerMover(
+            PlayerParam playerParam,
             PlayerCore playerCore,
-            PlayerInput playerInput,
-            PlanetStateMachine planetStateMachine,
-            PlanetController planet,
-            PlanetParams planetParams)
+            PlayerInput playerInput)
         {
             _playerParams = playerParam;
             _player = playerCore;
-            _planetParams = planetParams;
 
             playerInput.Jump
                 .Where(isJump => isJump)
                 .Subscribe(_ => Jump())
-                .AddTo(_player);
-
-            playerInput.Orbit
-                .Where(isOrbit => isOrbit &&
-                                  planetStateMachine.CurrentState == planetStateMachine.Deploy)
-                .Subscribe(_ => Orbit(planet.PlanetTransform.position))
                 .AddTo(_player);
 
             Observable.EveryUpdate(UnityFrameProvider.FixedUpdate)
@@ -44,10 +28,6 @@ namespace Orbit.Player
                             _player.Rb.linearVelocity.sqrMagnitude <= 0)
                 .Subscribe(_ => Move(playerInput.Move))
                 .AddTo(_player);
-        }
-
-        public void Tick()
-        {
         }
 
         private void Move(Vector2 moveInput)
@@ -66,60 +46,10 @@ namespace Orbit.Player
             _player.Rb.AddForce(Vector2.up * _playerParams.JumpForce, ForceMode2D.Impulse);
         }
 
-        private void Orbit(Vector2 planetPosition)
-        {
-            var colliders = GetCollidersInCircle(planetPosition, _planetParams.OrbitalRange, "Orbitable");
-            if (colliders.Length == 0 || _isOrbiting) return;
-
-            _isOrbiting = true;
-            foreach (var col in colliders)
-            {
-                CreateOrbitMotion(planetPosition, col.attachedRigidbody);
-            }
-        }
-
-        private Collider2D[] GetCollidersInCircle(Vector2 centerPosition, float radius, string layerTag)
-        {
-            var hitColliders = new Collider2D[20];
-            var filter = new ContactFilter2D();
-            filter.SetLayerMask(LayerMask.GetMask(layerTag));
-            filter.useTriggers = false;
-            var count = Physics2D.OverlapCircle(centerPosition, radius, filter, hitColliders);
-
-            return count == 0 ? Array.Empty<Collider2D>() : hitColliders.Take(count).ToArray();
-        }
-
-        private void CreateOrbitMotion(Vector2 centerPos, Rigidbody2D targetRb)
-        {
-            targetRb.simulated = false;
-
-            var dirToCenter = centerPos - targetRb.position;
-            var targetPos = targetRb.position + dirToCenter * 2;
-
-            // 移動モーション
-            LMotion.Create(targetRb.position, targetPos, 0.6f)
-                .WithEase(Ease.InSine)
-                .BindToLocalPositionXY(targetRb.transform);
-
-            // 拡大縮小モーション(手前に公転してるイメージ)
-            LMotion.Create(targetRb.transform.localScale, targetRb.transform.localScale * 1.5f, 0.3f)
-                .WithLoops(2, LoopType.Yoyo)
-                .WithEase(Ease.Linear)
-                .WithOnComplete(() =>
-                {
-                    targetRb.simulated = true;
-                    _isOrbiting = false;
-                    if (!targetRb.CompareTag("Player")) return;
-                    _player.CheckBuried();
-                    AddImpulse(dirToCenter.normalized * _planetParams.ReleaseForce);
-                })
-                .BindToLocalScale(targetRb.transform);
-        }
-
-        private void AddImpulse(Vector2 toPos)
+        public void AddImpulse(Vector2 toPos)
         {
             _player.Rb.linearVelocity = Vector2.zero;
             _player.Rb.AddForce(toPos, ForceMode2D.Impulse);
-        }
+        }  
     }
 }
