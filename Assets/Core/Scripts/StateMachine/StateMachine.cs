8da79aaadf5c107fd89f3e7d030f57640489239a
MwRano <noratohira@gmail.com>
2025-11-18 11:17:14 +0900

    StateMachineの基底クラスを作成

---
+++ Assets/Core/Scripts/StateMachine/StateMachine.cs
@@ -0,0 +1,32 @@
+#nullable enable
+using VContainer.Unity;
+
+namespace Core.StateMachine
+{
+    public abstract class StateMachine<TState, TStateMachine> : IInitializable, ITickable
+        where TState : class, IState<TStateMachine>
+        where TStateMachine : class
+    {
+        public TState CurrentState { get; private set; } = null!;
+
+        public abstract void Initialize();
+
+        public void Tick()
+        {
+            CurrentState.Update(this as TStateMachine ?? throw new System.InvalidCastException());
+        }
+
+        public void TransitionTo(TState nextState)
+        {
+            CurrentState?.Exit();
+            CurrentState = nextState;
+            CurrentState.Enter();
+        }
+
+        protected void SetInitialState(TState initialState)
+        {
+            CurrentState = initialState;
+            CurrentState.Enter();
+        }
+    }
+}
