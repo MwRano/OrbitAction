47a82b18695cced9d03684ef26f7d12b54ba4796
MwRano <noratohira@gmail.com>
2025-11-18 20:07:27 +0900

    パラメータの追加

--- Assets/Planet/Params/PlanetParams.cs
+++ Assets/Planet/Params/PlanetParams.cs
@@ -5,20 +5,26 @@ namespace Orbit.Planet
     [CreateAssetMenu(fileName = "PlanetParams", menuName = "Scriptable Objects/PlanetParams")]
     public class PlanetParams : ScriptableObject
     {
-        [Header("惑星の追従")] [SerializeField] private float smoothTime;
-
+        [Header("追従")] 
+        [SerializeField] private float smoothTime;
         [SerializeField] private float maxSpeed;
 
-        [Header("惑星の飛距離")] [SerializeField] private float launchDistance; // 惑星の飛距離
+        [Header("発射")] 
+        [SerializeField] private float launchDistance; // 惑星の飛距離
+        [SerializeField] private float launchTime; // 発射時間
 
-        [Header("公転")] [SerializeField] private float orbitalRange; // 公転可能な範囲
-
+        [Header("公転")] 
+        [SerializeField] private float orbitalRange; // 公転可能な範囲
+        [SerializeField] private float orbitalTime;
         [SerializeField] private float releaseForce; // 公転から離脱する際の力
 
         public float SmoothTime => smoothTime;
         public float MaxSpeed => maxSpeed;
         public float LaunchDistance => launchDistance;
-        public float ReleaseForce => releaseForce;
+        public float LaunchDistanceSqr => launchDistance * launchDistance;
+        public float LaunchTime => launchTime;
         public float OrbitalRange => orbitalRange;
+        public float OrbitalTime => orbitalTime;
+        public float ReleaseForce => releaseForce;
     }
 }
