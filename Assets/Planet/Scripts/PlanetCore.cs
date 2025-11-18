39bbf61643f3d9c7613cd1605b5704efefd500b2
MwRano <noratohira@gmail.com>
2025-11-18 19:59:11 +0900

    Planetの核クラスを作成

---
+++ Assets/Planet/Scripts/PlanetCore.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Orbit.Planet
+{
+    public class PlanetCore : MonoBehaviour
+    {
+        [SerializeField] private SpriteRenderer orbitableAreaView = null!;
+        
+        public SpriteRenderer PlanetView => GetComponent<SpriteRenderer>();
+        public SpriteRenderer OrbitableAreaView => orbitableAreaView;
+    }
+}
