e8a49ad7d9034bdb13bb3a23524d19d1f05e26d6
MwRano <noratohira@gmail.com>
2025-11-18 14:56:54 +0900

    Planetの入力を受け取るクラスを作成

---
+++ Assets/Planet/Scripts/PlanetInput.cs
@@ -0,0 +1,50 @@
+using System;
+using R3;
+using UnityEngine.InputSystem;
+using VContainer;
+
+namespace Orbit.Planet
+{
+    public class PlanetInput :IDisposable
+    {
+        private readonly InputSystemActions _inputSystemActions;
+        private readonly ReactiveProperty<bool> _orbit = new();
+        private readonly ReactiveProperty<bool> _launch = new();
+        
+        public ReadOnlyReactiveProperty<bool> Orbit => _orbit;
+        public ReadOnlyReactiveProperty<bool> Launch => _launch;
+
+        [Inject]
+        public PlanetInput(InputSystemActions inputSystemActions)
+        {
+            _inputSystemActions = inputSystemActions;
+            
+            _inputSystemActions.Planet.Orbit.performed += OnOrbit;
+            _inputSystemActions.Planet.Orbit.canceled += OnOrbit;
+            _inputSystemActions.Planet.Launch.performed += OnLaunch;
+            _inputSystemActions.Planet.Launch.canceled += OnLaunch;
+
+            _inputSystemActions.Player.Enable();
+        }
+
+        public void Dispose()
+        {
+            _inputSystemActions.Planet.Orbit.performed += OnOrbit;
+            _inputSystemActions.Planet.Orbit.canceled += OnOrbit;
+            _inputSystemActions.Planet.Launch.performed += OnLaunch;
+            _inputSystemActions.Planet.Launch.canceled += OnLaunch;
+
+            _inputSystemActions.Player.Enable();
+        }
+
+        private void OnOrbit(InputAction.CallbackContext context)
+        {
+            _orbit.Value = context.ReadValueAsButton();
+        }
+        
+        private void OnLaunch(InputAction.CallbackContext context)
+        {
+            _launch.Value = context.ReadValueAsButton();
+        }
+    }
+}
