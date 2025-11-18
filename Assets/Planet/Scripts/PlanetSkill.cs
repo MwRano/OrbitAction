70d76e5365f1aff3bbbcab4c6750d17b7427fd6b
MwRano <noratohira@gmail.com>
2025-11-18 20:01:36 +0900

    Planetのスキル関係を処理を行うクラスを作成

---
+++ Assets/Planet/Scripts/PlanetSkill.cs
@@ -0,0 +1,131 @@
+using System;
+using System.Linq;
+using LitMotion;
+using LitMotion.Extensions;
+using Orbit.Player;
+using R3;
+using UnityEngine;
+using VContainer;
+using VContainer.Unity;
+
+namespace Orbit.Planet
+{
+    public class PlanetSkill : ITickable
+    {
+        private readonly PlanetParams _planetParams;
+        private readonly PlanetCore _planet;
+        private readonly PlayerCore _player;
+        private readonly PlayerMover _playerMover;
+        private readonly DeployPositionCalculator _deployPositionCalculator;
+        private readonly PlayerAimer _playerAimer;
+        private bool _isOrbiting;
+        
+        [Inject]
+        public PlanetSkill(
+            PlanetParams planetParams,
+            PlanetCore planet,
+            PlayerCore player,
+            PlayerMover playerMover,
+            PlanetInput planetInput,
+            DeployPositionCalculator deployPositionCalculator,
+            PlayerAimer playerAimer,
+            PlanetStateMachine planetStateMachine)
+        {
+            _planetParams = planetParams;
+            _planet = planet;
+            _player = player;
+            _playerMover = playerMover;
+            _deployPositionCalculator = deployPositionCalculator;
+            _playerAimer = playerAimer;
+            
+            planetInput.Orbit
+                .Where(isOrbit => isOrbit &&
+                                  planetStateMachine.CurrentState == planetStateMachine.Deploy)
+                .Subscribe(_ => Orbit())
+                .AddTo(planet);
+            
+            planetInput.Launch
+                .Where(isLaunch => isLaunch &&
+                                   planetStateMachine.CurrentState == planetStateMachine.Hover　||
+                                   planetStateMachine.CurrentState == planetStateMachine.Follow)
+                .Subscribe(_ => Launch())
+                .AddTo(planet);
+        }
+
+        public void Tick()
+        {
+        }
+
+        private void Launch()
+        {
+            // 移動モーション 
+            var planetRadius = _planet.PlanetView.bounds.extents.x;
+            var destPos = _deployPositionCalculator.Calculate(
+                _player.transform.position,
+                _playerAimer.AimDirection,
+                _planet.transform.position,
+                planetRadius
+            );
+
+            // deply地点にlaunch
+            LMotion.Create((Vector2)_planet.transform.position, destPos, _planetParams.LaunchTime)
+                .WithEase(Ease.OutCubic)
+                .BindToPositionXY(_planet.transform)
+                .AddTo(_planet);
+        }
+
+        private void Orbit()
+        {
+            var colliders = GetCollidersInCircle(
+                _planet.transform.position, 
+                _planetParams.OrbitalRange, 
+                "Orbitable");
+            if (colliders.Length == 0 || _isOrbiting) return;
+
+            _isOrbiting = true;
+            foreach (var col in colliders)
+            {
+                CreateOrbitMotion(_planet.transform.position, col.attachedRigidbody);
+            }
+            
+        }
+
+        private Collider2D[] GetCollidersInCircle(Vector2 centerPosition, float radius, string layerTag)
+        {
+            var hitColliders = new Collider2D[20];
+            var filter = new ContactFilter2D();
+            filter.SetLayerMask(LayerMask.GetMask(layerTag));
+            filter.useTriggers = false;
+            var count = Physics2D.OverlapCircle(centerPosition, radius, filter, hitColliders);
+
+            return count == 0 ? Array.Empty<Collider2D>() : hitColliders.Take(count).ToArray();
+        }
+
+        private void CreateOrbitMotion(Vector2 centerPos, Rigidbody2D targetRb)
+        {
+            targetRb.simulated = false;
+
+            var dirToCenter = centerPos - targetRb.position;
+            var targetPos = targetRb.position + dirToCenter * 2;
+
+            // 移動モーション
+            LMotion.Create(targetRb.position, targetPos, _planetParams.OrbitalTime)
+                .WithEase(Ease.InSine)
+                .BindToLocalPositionXY(targetRb.transform);
+
+            // 拡大縮小モーション(手前に公転してるイメージ)
+            LMotion.Create(targetRb.transform.localScale, targetRb.transform.localScale * 1.5f, _planetParams.OrbitalTime / 2)
+                .WithLoops(2, LoopType.Yoyo)
+                .WithEase(Ease.Linear)
+                .WithOnComplete(() =>
+                {
+                    targetRb.simulated = true;
+                    _isOrbiting = false;
+                    if (!targetRb.CompareTag("Player")) return;
+                    _player.CheckBuried();
+                    _playerMover.AddImpulse(dirToCenter.normalized * _planetParams.ReleaseForce);
+                })
+                .BindToLocalScale(targetRb.transform);
+        }
+    }
+}
--- Assets/Planet/Scripts/State/Deploy.cs
+++ Assets/Planet/Scripts/State/Deploy.cs
@@ -1,8 +1,7 @@
-﻿#nullable enable
+﻿using System;
 using LitMotion;
 using LitMotion.Extensions;
 using Orbit.Core.StateMachine;
-using Orbit.Player;
 using UnityEngine;
 using VContainer;
 
@@ -13,64 +12,67 @@ namespace Orbit.Planet.State
     /// </summary>
     public class Deploy : IState<PlanetStateMachine>
     {
-        private readonly CompositeMotionHandle _handles = new();
         private readonly PlanetParams _planetParams;
-        private readonly PlayerCore _player;
         private MotionHandle _floatingMotion;
-        private bool _isOrbiting;
-        private GameObject _orbitAreaView = null!;
-        private readonly PlanetController _planet;
+        private readonly PlanetCore _planet;
+        private readonly PlanetInput _planetInput;
 
         [Inject]
         public Deploy(
             PlanetParams planetParams, 
-            PlayerCore player,
-            PlanetController planet)
+            PlanetCore planet,
+            PlanetInput planetInput)
         {
-            _player = player;
             _planetParams = planetParams;
             _planet = planet;
+            _planetInput = planetInput;
         }
 
         public void Enter()
         {
             // 浮遊モーション
             _floatingMotion = LMotion
-                .Create(_planet.PlanetTransform.position.y, _planet.PlanetTransform.position.y - 0.2f, 1f)
+                .Create(_planet.transform.position.y, _planet.transform.position.y - 0.2f, 1f)
                 .WithEase(Ease.InOutSine)
                 .WithLoops(-1, LoopType.Yoyo)
-                .BindToPositionY(_planet.PlanetTransform)
-                .AddTo(_planet.PlanetTransform);
-
-            // 公転範囲表示
-            _orbitAreaView = _planet.OrbitAreaView;
-
+                .BindToPositionY(_planet.transform)
+                .AddTo(_planet);
+            
             // 拡大モーション
-            float baseRadius = _planet.OrbitAreaSpriteRenderer.sprite.bounds.extents.x;
-            _orbitAreaView.SetActive(true);
-            LMotion.Create(Vector2.zero, Vector2.one * (_planetParams.OrbitalRange / baseRadius), 0.3f)
-                .WithEase(Ease.OutBack)
-                .BindToLocalScaleXY(_orbitAreaView.transform)
-                .AddTo(_handles);
+            _planet.OrbitableAreaView.enabled = true;
+            var baseRadius = _planet.OrbitableAreaView.sprite.bounds.extents.x;
+            CreateScaleMotion(Vector2.zero, Vector2.one * (_planetParams.OrbitalRange / baseRadius));
         }
-
+        
         public void Update(PlanetStateMachine stateMachine)
         {
             // 状態遷移の判定
-            if (!_planet.IsLaunched && !_player.IsGoalReached.CurrentValue)
+            if (!_planetInput.Launch.CurrentValue)
                 stateMachine.TransitionTo(stateMachine.Follow);
         }
-
+        
         public void Exit()
         {
             _floatingMotion.Cancel();
-
+            
             // 縮小モーション
-            LMotion.Create((Vector2)_orbitAreaView.transform.localScale, Vector2.zero, 0.3f)
-                .WithEase(Ease.OutBack)
-                .WithOnComplete(() => _orbitAreaView.SetActive(false))
-                .BindToLocalScaleXY(_orbitAreaView.transform)
-                .AddTo(_handles);
+            CreateScaleMotion(_planet.OrbitableAreaView.transform.localScale, 
+                Vector2.zero,
+                () => _planet.OrbitableAreaView.enabled = false);
+        }
+        
+        private void CreateScaleMotion(Vector2 fromScale, Vector2 toScale, Action onComplete = null)
+        {
+            var motionBuilder = LMotion.Create(fromScale, toScale, 0.3f)
+                .WithEase(Ease.OutBack);
+            
+            if (onComplete != null)
+            {
+                motionBuilder = motionBuilder.WithOnComplete(onComplete);
+            }
+            
+            motionBuilder.BindToLocalScaleXY(_planet.OrbitableAreaView.transform)
+                .AddTo(_planet.OrbitableAreaView);
         }
     }
 }
