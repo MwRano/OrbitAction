a64d7eeb17c8a33ade6421a1562b0ddefc167aa9
MwRano <noratohira@gmail.com>
2025-11-21 15:18:34 +0900

    fadeを実行するためのクラスを作成

---
+++ Assets/Game/Scripts/ScreenFader.cs
@@ -0,0 +1,35 @@
+﻿using LitMotion;
+using UnityEngine;
+using VContainer;
+
+namespace Orbit.Game
+{
+    public class ScreenFader
+    {
+        private static readonly int FadeAmount = Shader.PropertyToID("_FadeAmount");
+        private static readonly int FlipX = Shader.PropertyToID("_FlipX");
+        private readonly FadeParams _fadeParams;
+
+        [Inject]
+        public ScreenFader(FadeParams fadeParams)
+        {
+            _fadeParams = fadeParams;
+        }
+
+        public void FadeIn()
+        {
+            LMotion.Create(1.0f, 0f, 0.5f)
+                .WithEase(Ease.Linear)
+                .Bind(value => _fadeParams.FadeMat.SetFloat(FadeAmount, value));
+        }
+
+        public void FadeOut()
+        {
+            _fadeParams.FadeMat.SetInt(FlipX, 1);
+            LMotion.Create(0, 1.0f, 0.5f)
+                .WithEase(Ease.Linear)
+                .WithOnComplete(() => _fadeParams.FadeMat.SetInt(FlipX, 0))
+                .Bind(value => _fadeParams.FadeMat.SetFloat(FadeAmount, value));
+        }
+    }
+}
