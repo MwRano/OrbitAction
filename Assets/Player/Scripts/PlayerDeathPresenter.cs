a3a3beae5e8ab4df292c331f808477e8a03e3488
MwRano <noratohira@gmail.com>
2025-11-21 15:20:12 +0900

    リスポーン状態とfade演出の仲介をするpresenterを作成

---
+++ Assets/Player/Scripts/PlayerDeathPresenter.cs
@@ -0,0 +1,31 @@
+﻿using Orbit.Game;
+using R3;
+using VContainer;
+using VContainer.Unity;
+
+namespace Orbit.Player
+{
+    public class PlayerDeathPresenter : IInitializable
+    {
+        [Inject]
+        public PlayerDeathPresenter(
+            PlayerCore player,
+            PlayerRespawner playerRespawner, 
+            ScreenFader screenFader)
+        {
+            playerRespawner.IsRespawning
+                .Where(isRespawning => isRespawning)
+                .Subscribe(_ => screenFader.FadeOut())
+                .AddTo(player);
+
+            playerRespawner.IsRespawning
+                .Where(isRespawning => !isRespawning)
+                .Subscribe(_ => screenFader.FadeIn())
+                .AddTo(player);
+        }
+
+        public void Initialize()
+        {
+        }
+    }
+}
